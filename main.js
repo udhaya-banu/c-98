b62b50f8a94a8c06ef29a983cee4a63bb296fa93
udhaya-banu <89130488+udhaya-banu@users.noreply.github.com>
2022-03-21 20:53:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var speechrecognition=window.webkitSpeechRecognition;
+var recognition=new speechrecognition();
+function begin(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=content;
+}
